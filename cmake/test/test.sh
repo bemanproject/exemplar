0c9a3e5b6ff219d7fb0349550e2eb2bfdb3566c2
Chris Hayden <ViperX883@gmail.com>
2025-02-12 16:46:17 +0000

    That didn't work. Try forwarding the CMake command.

--- cmake/test/test.sh
+++ cmake/test/test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -x
 
-cmake -S "${1}" -B "${2}" \
-      -DCMAKE_PREFIX_PATH="${3}" \
-      -DCMAKE_CXX_STANDARD=17
-
-cmake --build "${2}" --config Debug
+"${1}" -S "${2}" -B "${3}" -DCMAKE_PREFIX_PATH="${4}"
+cmake --build "${3}" --config Debug
