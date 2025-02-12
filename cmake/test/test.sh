a133cb8d5267bc2505cdb1ec2023aadec10dfefb
Chris Hayden <ViperX883@gmail.com>
2025-02-12 11:37:22 -0500

    Specify a CXX standard to fix compile errors for some toolchains.

--- cmake/test/test.sh
+++ cmake/test/test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
 
-cmake -S "${1}" -B "${2}" -DCMAKE_PREFIX_PATH="${3}"
+cmake -S "${1}" -B "${2}" \
+      -DCMAKE_PREFIX_PATH="${3}" \
+      -DCMAKE_CXX_STANDARD=17
+
 cmake --build "${2}" --config Debug
