91213c25af90ef3399b825018e74317b1ff841d3
Chris Hayden <ViperX883@gmail.com>
2025-02-14 12:11:16 -0500

    Fix multi-line command syntax.

--- cmake/test/test.bat
+++ cmake/test/test.bat
@@ -1,6 +1,6 @@
-"%1" -S "%2" -B "%3" \
-       -DCMAKE_PREFIX_PATH="%4" \
-       -DCMAKE_BUILD_TYPE="%5" \
+"%1" -S "%2" -B "%3" ^
+       -DCMAKE_PREFIX_PATH="%4" ^
+       -DCMAKE_BUILD_TYPE="%5" ^
        -DCMAKE_CXX_STANDARD=17
 
 "%1" --build "%3" --config "%5"
