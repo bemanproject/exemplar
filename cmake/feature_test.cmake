045154c93a512f55c50be6acc5e0a9eeae6deb0d
River <26424577+wusatosi@users.noreply.github.com>
2025-05-29 14:17:31 -0400

    simplify feature_test per suggestion

--- cmake/feature_test.cmake
+++ cmake/feature_test.cmake
@@ -1,54 +1,14 @@
 include(CheckCXXSourceCompiles)
 
-function(BEMAN_FEATURE_TEST FEATURE_TEST_EXP RESULT_VAR)
+function(beman_exemplar_check_cpp_17_support result_var)
     check_cxx_source_compiles(
         "
-    #if (${FEATURE_TEST_EXP})
-    int main() {}
-    #else
+    #if (defined(__cplusplus) && __cplusplus < 201703L) || (defined(_MSVC_LANG) && _MSVC_LANG < 201703L)
     #error
     #endif
+
+    int main() {}
     "
-        ${RESULT_VAR}
+        ${result_var}
     )
-endfunction()
-
-function(BEMAN_MIN_CXX_VERSION CXX_VERSION)
-    set(VERSION_MAP
-        11
-        201103L
-        14
-        201402L
-        17
-        201703L
-        20
-        202002L
-        23
-        202302L
-    )
-
-    list(FIND VERSION_MAP "${CXX_VERSION}" INDEX)
-    if(INDEX EQUAL -1)
-        message(FATAL_ERROR "Unknown required C++ version: ${CXX_VERSION}")
-    else()
-        math(EXPR INDEX "${INDEX} + 1")
-        list(GET VERSION_MAP ${INDEX} VERSION_DATE)
-    endif()
-
-    beman_feature_test("__cplusplus >= ${VERSION_DATE}" CXX_VERSION_MET)
-    if(NOT CXX_VERSION_MET)
-        if(MSVC)
-            message(
-                WARNING
-                "Note: C++ version support detection requires a well-formed __cplusplus feature flag, \
-            MSVC does not set this flag correctly unless /Zc:__cplusplus flag is passed through.\
-            If you believe minimum C++ version detection is function incorrectly,\
-            please double check your compiler flags or use the provided presets if available."
-            )
-        endif()
-        message(
-            FATAL_ERROR
-            "This project requires a minimum C++ version of ${CXX_VERSION}"
-        )
-    endif()
 endfunction()
