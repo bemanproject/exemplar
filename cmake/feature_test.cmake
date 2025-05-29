d411fd0b4ee1169185b1aa971b9eb607d4f879e4
River <26424577+wusatosi@users.noreply.github.com>
2025-05-29 14:26:05 -0400

    simplify feature test expression

--- cmake/feature_test.cmake
+++ cmake/feature_test.cmake
@@ -3,7 +3,7 @@ include(CheckCXXSourceCompiles)
 function(beman_exemplar_check_cpp_17_support result_var)
     check_cxx_source_compiles(
         "
-    #if (defined(__cplusplus) && __cplusplus < 201703L) || (defined(_MSVC_LANG) && _MSVC_LANG < 201703L)
+    #if !(__cplusplus >= 201703L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L))
     #error
     #endif
 
