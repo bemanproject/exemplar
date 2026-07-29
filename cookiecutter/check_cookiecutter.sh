bemanproject/exemplar

commit 436eea402499e1599b2f3f0e7570ddb695a3eab4

lint: add missing SPDX license identifiers
to enforce file.license_id from the beman standard

cookiecutter/check_cookiecutter.sh
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #!/usr/bin/env bash
 
 set -euo pipefail

.github/workflows/pre-commit-check.yml
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 name: Lint Check (pre-commit)
 
 on:

.markdownlint.yaml
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # MD033/no-inline-html : Inline HTML : https://github.com/DavidAnson/markdownlint/blob/v0.35.0/doc/md033.md
 # Disable inline html linter is needed for <details> <summary>
 MD033: false

.pre-commit-config.yaml
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # See https://pre-commit.com for more information
 # See https://pre-commit.com/hooks.html for more hooks
 repos:

cookiecutter/{{cookiecutter.project_name}}/.github/workflows/pre-commit-check.yml
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 name: Lint Check (pre-commit)
 
 on:

cookiecutter/{{cookiecutter.project_name}}/.markdownlint.yaml
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # MD033/no-inline-html : Inline HTML : https://github.com/DavidAnson/markdownlint/blob/v0.35.0/doc/md033.md
 # Disable inline html linter is needed for <details> <summary>
 MD033: false

cookiecutter/{{cookiecutter.project_name}}/.pre-commit-config.yaml
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # See https://pre-commit.com for more information
 # See https://pre-commit.com/hooks.html for more hooks
 repos:

cookiecutter/{{cookiecutter.project_name}}/examples/identity_direct_usage.cpp
@@ -1,5 +1,5 @@
-{% set identity = "identity" if cookiecutter._generating_exemplar else "todo" %}
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+{% set identity = "identity" if cookiecutter._generating_exemplar else "todo" %}
 
 #include <beman/{{cookiecutter.project_name}}/{{identity}}.hpp>
 

commit 6ad4f4156481ebbea12e54e589dca8381749d815

Fix check_cookiecutter.sh hashbang
This was broken by commit 436eea402499e1599b2f3f0e7570ddb695a3eab4
(https://github.com/bemanproject/exemplar/pull/375) adding an
SPDX-License-Identifier commit on line 1.

cookiecutter/check_cookiecutter.sh
@@ -1,5 +1,5 @@
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 set -euo pipefail
 